e7360fdd7d0d9ae5451b1f641c51f53b8995f3e8
Murilo Ribeiro <88835239+MuriloRibg@users.noreply.github.com>
2023-08-15 01:14:06 -0300

    Adiciona o nodemon o scss e tela de message inicial

--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,21 @@
-var express = require('express'); 
+var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
     res.render('index');
+});
+
+router.get('/message', (req, res, next) => {
+    res.render('node', { message: "Nova página." });
+});
+
+router.post('/message', (req, res, next) => {
+    var messageVar = req.body.messageBody
+    res.redirect('/message/' + messageVar);
+});
+
+router.get('/message/:msgParam', (req, res, next) => {
+    res.render('node', { message: req.params.msgParam });
 });
 
 module.exports = router; 
